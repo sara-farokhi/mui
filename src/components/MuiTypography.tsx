567ba5264f2cb1d4078e3ba2ba15a121832e2dac
sara-farokhi <sarafarokhi.sf@gmail.com>
2022-12-12 02:27:16 -0800

    mui typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,30 @@
+import Typography from '@mui/material/Typography';
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant='h1'> h1  Heading</Typography>
+      <Typography variant='h2'> h2  Heading</Typography>
+      <Typography variant='h3'> h3  Heading</Typography>
+      <Typography variant='h4'> h4  Heading</Typography>
+      <Typography variant='h5'> h5  Heading</Typography>
+      <Typography variant='h6'> h6  Heading</Typography>
+      <Typography variant='subtitle1'>subtitle1 </Typography>
+      <Typography variant='subtitle2'> subtitle2</Typography>
+      <Typography variant='body1'> body1</Typography>
+      <Typography variant='body2'> body2</Typography>
+      <Typography variant="button" display="block">
+        button text
+      </Typography>
+      <Typography variant="caption" display="block">
+        caption text
+      </Typography>
+      <Typography variant="overline" display="block">
+        overline text
+      </Typography>
+
+
+    </div>
+  )
+}
+
+export default MuiTypography
