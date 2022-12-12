6aa52a74c1e8d6973fecf1a92a52f149d6a1fe21
sara-farokhi <sarafarokhi.sf@gmail.com>
2022-12-12 08:20:23 -0800

    Mui Select

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 // import MuiTypography from "./components/MuiTypography"
 // import MuiButton from "./components/MuiButton"
-import MuiTextField from "./components/MuiTextField"
+// import MuiTextField from "./components/MuiTextField"
+import MuiSelect from "./components/MuiSelect"
+
+
 
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
-      <MuiTextField />
+      {/* <MuiTextField /> */}
+      <MuiSelect />
     </div>
   );
 }
---
+++ src/components/MuiSelect.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Box, TextField, MenuItem } from "@mui/material"
+
+const MuiSelect = () => {
+  const [value, setValue] = useState("")
+  const [values, setValues] = useState<string[]>([])
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+  const handleMultiChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const multiValues = e.target.value
+    setValues(typeof multiValues === "string" ? multiValues.split(",") : multiValues)
+
+  }
+
+  return (
+    <>
+      <Box width="300px" m={2}>
+        <TextField label="Countries" value={value} select fullWidth onChange={handleChange}>
+          <MenuItem value="ir">Iran</MenuItem>
+          <MenuItem value="us">USA</MenuItem>
+          <MenuItem value="uk">UK</MenuItem>
+        </TextField>
+      </Box>
+      <Box width="300px" m={2}>
+        <TextField label="Countries" value={values} select fullWidth onChange={handleMultiChange}
+          SelectProps={{ multiple: true }}>
+          <MenuItem value="ir">Iran</MenuItem>
+          <MenuItem value="us">USA</MenuItem>
+          <MenuItem value="uk">UK</MenuItem>
+        </TextField>
+      </Box>
+    </>
+  )
+}
+
+export default MuiSelect
